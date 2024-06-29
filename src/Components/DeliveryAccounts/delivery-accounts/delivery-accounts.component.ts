61b19089b22d0f1131c1f98d7954f4f9738bcacd
Hala Mansour <halamansour888222555@gmail.com>
2024-06-30 02:12:51 +0300

    Pagination Added

--- src/Components/DeliveryAccounts/delivery-accounts/delivery-accounts.component.ts
+++ src/Components/DeliveryAccounts/delivery-accounts/delivery-accounts.component.ts
@@ -4,27 +4,62 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { InputSwitchModule } from 'primeng/inputswitch';
 import { FormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { PaginatorModule } from 'primeng/paginator';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-delivery-accounts',
   standalone: true,
-  imports: [CommonModule,RouterLink,RouterLinkActive, InputSwitchModule,FormsModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    InputSwitchModule,
+    FormsModule,
+    TableModule,
+    PaginatorModule,
+    InputTextModule
+  ],
   templateUrl: './delivery-accounts.component.html',
-  styleUrl: './delivery-accounts.component.css'
+  styleUrls: ['./delivery-accounts.component.css']
 })
 export class DeliveryAccountsComponent implements OnInit {
+  deliveryAccounts: any[] = [];
+  totalRecords: number = 0;
+  loading: boolean = false;
+  first: number = 0;
+  rows: number = 10;
+  searchValue: string = '';
 
-  constructor(private _DeliveryService : DeliveryService){ }
-  
-  deliveryAccounts :any[]=[];
+  constructor(private _DeliveryService: DeliveryService) {}
 
   ngOnInit(): void {
+    this.loadDeliveryAccounts();
+  }
+
+  loadDeliveryAccounts(): void {
+    this.loading = true;
     this._DeliveryService.getDeliveryAccounts().subscribe({
-      next:(Response) =>{
-       // console.log(Response)
-        this.deliveryAccounts=Response;
-       // console.log(this.deliveryAccounts);
+      next: (response) => {
+        this.deliveryAccounts = response;
+        this.totalRecords = this.deliveryAccounts.length; 
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Error loading delivery accounts', err);
+        this.loading = false;
       }
-    })
+    });
+  }
+
+  
+  onSwitchChange(event: any) {
+    console.log('Switch state:', event.checked);
+  }
+
+  onPageChange(event: any): void {
+    this.first = event.first;
+    this.rows = event.rows;
   }
 }
