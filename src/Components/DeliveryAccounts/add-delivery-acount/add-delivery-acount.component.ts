6ffb42a2719e9e2823967181f529536163bdea48
Hala Mansour <halamansour888222555@gmail.com>
2024-06-30 01:25:43 +0300

    Display Delivery Accounts

--- src/Components/DeliveryAccounts/add-delivery-acount/add-delivery-acount.component.ts
+++ src/Components/DeliveryAccounts/add-delivery-acount/add-delivery-acount.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-add-delivery-acount',
   standalone: true,
-  imports: [],
+  imports: [RouterLink , RouterLinkActive],
   templateUrl: './add-delivery-acount.component.html',
   styleUrl: './add-delivery-acount.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,17 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from '../Components/layout/layout.component';
 import { MainDashboardComponent } from '../Components/main-dashboard/main-dashboard.component';
 import { OrderFormComponent } from '../Components/Order/order-form/order-form.component';
+import { DeliveryAccountsComponent } from '../Components/DeliveryAccounts/delivery-accounts/delivery-accounts.component';
+import { MerchantAccountsComponent } from '../Components/MercgentAccount/merchant-accounts/merchant-accounts.component';
+import { AddDeliveryAcountComponent } from '../Components/DeliveryAccounts/add-delivery-acount/add-delivery-acount.component';
 
 export const routes: Routes = [
 
     {path:'',component:LayoutComponent,children:[
         {path:'',component:MainDashboardComponent},
-        {path:'addorder',component:OrderFormComponent}
+        {path:'addorder',component:OrderFormComponent},
+        {path:'DeliveryAccounts',component:DeliveryAccountsComponent },
+        {path:'MerchantAccounts',component:MerchantAccountsComponent},
+        {path:'AddDeliveryAccount' ,component:AddDeliveryAcountComponent}
     ]}
 ];
