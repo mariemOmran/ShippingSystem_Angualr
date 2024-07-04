389f348c5b668df46722be3f6953e6c29f297401
AzzaMohamedd <81493771+AzzaMohamedd@users.noreply.github.com>
2024-07-04 16:33:57 +0300

    add login design

--- src/Components/login/login.component.ts
+++ src/Components/login/login.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+
+import { Component, AfterViewInit, ViewEncapsulation } from '@angular/core';
+// import Swiper, { EffectCards, Keyboard, Pagination } from 'swiper';
+// import 'swiper/swiper-bundle.css';
+
+// Swiper.use([EffectCards, Keyboard, Pagination]);
+declare function initializeSwiper(): void;
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  encapsulation: ViewEncapsulation.None // Use None to allow global styles to be applied
 })
-export class LoginComponent {
 
+export class LoginComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    initializeSwiper();
+  }
 }
