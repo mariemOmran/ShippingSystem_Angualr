37e21391b60bcec81da1cd16dca57300d5ad1c1b
AzzaMohamedd <81493771+AzzaMohamedd@users.noreply.github.com>
2024-07-09 16:50:29 +0300

    login validations

--- src/Components/login/login.component.ts
+++ src/Components/login/login.component.ts
@@ -3,14 +3,11 @@ import { CommonModule } from '@angular/common';
 import { Component, AfterViewInit, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthServiceService } from '../../Services/auth-service.service';
-import { emailPasswordValidator } from '../../Validators/emailPasswordValidator';
+// import { emailPasswordValidator } from '../../Validators/emailPasswordValidator';
 import { ILogin } from '../../Models/i-login';
 import { Router } from '@angular/router';
 import { GlobalService } from '../../Services/global.service';
-// import Swiper, { EffectCards, Keyboard, Pagination } from 'swiper';
-// import 'swiper/swiper-bundle.css';
 
-// Swiper.use([EffectCards, Keyboard, Pagination]);
 declare function initializeSwiper(): void;
 
 @Component({
@@ -36,7 +33,8 @@ export class LoginComponent implements AfterViewInit {
     Email: new FormControl('', [Validators.required, Validators.email]),
     Password: new FormControl('', [Validators.required])
   }, 
-  { asyncValidators: emailPasswordValidator(this.authService,'Employee') });
+  // { asyncValidators: emailPasswordValidator(this.authService,'Employee') }
+);
   
   deliveryLoginForm: FormGroup = new FormGroup({
     Email: new FormControl('', [Validators.required, Validators.email]),
@@ -59,40 +57,64 @@ export class LoginComponent implements AfterViewInit {
     // this.isLoading = true;
 
     if(role=="delivery"){
+      if (!this.deliveryLoginForm.valid) {
+        this.deliveryLoginForm.markAllAsTouched();
+      }
       const { Email, Password } = this.deliveryLoginForm.value;
-      this.accObj={email:Email,password:Password};
 
-      this.authService.login(this.accObj,'Delivery').subscribe({
-        next: (res: any) => {
-          if(res=="Email Not Valid"){
-            this.deliveryLoginForm.get('Email')?.setErrors({ 'invalidEmailLogin': true });
-          }
-          else if(res=="password Not Valid"){
-            this.deliveryLoginForm.get('Password')?.setErrors({ 'invalidPasswordLogin': true });
-          }
-          else{  //email and password are correct
-            const jsonObject = JSON.parse(res);
-            localStorage.setItem('token', jsonObject.readLoginDTO.token);
-            this.globalService.loadGlobalData().then(() => {
+      if (Email=="") {        
+        this.deliveryLoginForm.get('Email')?.setErrors({ 'required': true });
+      } 
+      if (Password=="") {
+        this.deliveryLoginForm.get('Password')?.setErrors({ 'required': true });
+      } 
+      if(this.deliveryLoginForm.valid){
+        this.accObj={email:Email,password:Password};
+        this.authService.login(this.accObj,'Delivery').subscribe({
+          next: (res: any) => {
+            var response = JSON.parse(res);
+            if(response.msg =="Email Not Valid"){
+              this.deliveryLoginForm.get('Email')?.setErrors({ 'invalidEmailLogin': true });
+            }
+            else if(response.msg =="password Not Valid"){
+              this.deliveryLoginForm.get('Password')?.setErrors({ 'invalidPasswordLogin': true });
+            }
+            else{  //email and password are correct
+              const jsonObject = JSON.parse(res);
+              localStorage.setItem('token', jsonObject.readLoginDTO.token);
+              this.globalService.loadGlobalData().then(() => {
+                this.router.navigate(['/']);
+              });
+            
+              debugger;
+
+
               this.router.navigate(['/']);
-            });
-          
-
-
-            this.router.navigate(['/']);
+            }
+          },
+          error: (error) => {
+            console.error('Login failed:', error);
+            this.deliveryLoginForm.get('Password')?.setErrors({ 'somethingWrong': true });
           }
-        },
-        error: (error) => {
-          console.error('Login failed:', error);
-          this.deliveryLoginForm.get('Password')?.setErrors({ 'somethingWrong': true });
-        }
-      });
+        });
+      }
     }
     else if(role=="employee"){
+      if (!this.empLoginForm.valid) {
+        this.empLoginForm.markAllAsTouched();
+      }
       const { Email, Password } = this.empLoginForm.value;
-      this.accObj={email:Email,password:Password};
 
-      this.authService.login(this.accObj,'Employee').subscribe({
+      if (Email=="") {        
+        this.empLoginForm.get('Email')?.setErrors({ 'required': true });
+      } 
+      if (Password=="") {
+        this.empLoginForm.get('Password')?.setErrors({ 'required': true });
+      } 
+      if(this.empLoginForm.valid){
+        this.accObj={email:Email,password:Password};
+
+        this.authService.login(this.accObj,'Employee').subscribe({
         next: (res: any) => {
           if(res=="Email Not Valid"){
             this.empLoginForm.get('Email')?.setErrors({ 'invalidEmailLogin': true });
@@ -116,37 +138,50 @@ export class LoginComponent implements AfterViewInit {
           console.error('Login failed:', error);
           this.empLoginForm.get('Password')?.setErrors({ 'somethingWrong': true });
         }
-      });
+        });
+      }
     }
     else if(role =="merchant"){
+      if (!this.merchantLoginForm.valid) {
+        this.merchantLoginForm.markAllAsTouched();
+      }
       const { Email, Password } = this.merchantLoginForm.value;
-      this.accObj={email:Email,password:Password};
 
-      this.authService.login(this.accObj,'Merchant').subscribe({
-        next: (res: any) => {
-          if(res=="Email Not Valid"){
-            this.merchantLoginForm.get('Email')?.setErrors({ 'invalidEmailLogin': true });
-          }
-          else if(res=="password Not Valid"){
-            this.merchantLoginForm.get('Password')?.setErrors({ 'invalidPasswordLogin': true });
-          }
-          else{  //email and password are correct
-            const jsonObject = JSON.parse(res);
-            localStorage.setItem('token', jsonObject.readLoginDTO.token);
-            this.globalService.loadGlobalData().then(() => {
+      if (Email=="") {        
+        this.merchantLoginForm.get('Email')?.setErrors({ 'required': true });
+      } 
+      if (Password=="") {
+        this.merchantLoginForm.get('Password')?.setErrors({ 'required': true });
+      } 
+      if(this.merchantLoginForm.valid){
+        this.accObj={email:Email,password:Password};
+
+        this.authService.login(this.accObj,'Merchant').subscribe({
+          next: (res: any) => {
+            if(res=="Email Not Valid"){
+              this.merchantLoginForm.get('Email')?.setErrors({ 'invalidEmailLogin': true });
+            }
+            else if(res=="password Not Valid"){
+              this.merchantLoginForm.get('Password')?.setErrors({ 'invalidPasswordLogin': true });
+            }
+            else{  //email and password are correct
+              const jsonObject = JSON.parse(res);
+              localStorage.setItem('token', jsonObject.readLoginDTO.token);
+              this.globalService.loadGlobalData().then(() => {
+                this.router.navigate(['/']);
+              });
+        
+
+
               this.router.navigate(['/']);
-            });
-      
-
-
-            this.router.navigate(['/']);
+            }
+          },
+          error: (error) => {
+            console.error('Login failed:', error);
+            this.merchantLoginForm.get('Password')?.setErrors({ 'somethingWrong': true });
           }
-        },
-        error: (error) => {
-          console.error('Login failed:', error);
-          this.merchantLoginForm.get('Password')?.setErrors({ 'somethingWrong': true });
-        }
-      });
+        });
+      }
     }
      
   }
--- src/Validators/emailPasswordValidator.ts
+++ src/Validators/emailPasswordValidator.ts
@@ -6,31 +6,31 @@ import { ILogin } from '../Models/i-login';
 
 export function emailPasswordValidator(authService: AuthServiceService,role:string): AsyncValidatorFn {
   return (control: AbstractControl): Observable<ValidationErrors | null> => {
-    const email = control.get('Email')?.value;
-    const password = control.get('Password')?.value;
-    let empData:ILogin={email:email,password:password}
+    // const email = control.get('Email')?.value;
+    // const password = control.get('Password')?.value;
+    // let empData:ILogin={email:email,password:password}
 
-    if (!email || !password) {
-      console.log("nulll");
-      return of(null); 
-    }
+    // if (!email || !password) {
+    //   console.log("nulll");
+    //   return of(null); 
+    // }
 
     
-    return authService.login(empData,role).pipe(
-      map(r => {   
-        var response = JSON.parse(r);
-        if(response.msg == "Email Not Valid") {
-          return { invalidEmailLogin: 'Invalid email. Please try again.' };
-        } else if(response.msg === "password Not Valid") {
-          return { invalidPasswordLogin: 'Invalid password. Please try again.' };
-        } else {  //email and password correct
-          return null;
-        }
-      }),
-      catchError(error => {
-        return of({ somethingWrong: 'Invalid response format. Please try again.' });
-      })
-    );
-    
+    // return authService.login(empData,role).pipe(
+    //   map(r => {   
+    //     var response = JSON.parse(r);
+    //     if(response.msg == "Email Not Valid") {
+    //       return { invalidEmailLogin: 'Invalid email. Please try again.' };
+    //     } else if(response.msg === "password Not Valid") {
+    //       return { invalidPasswordLogin: 'Invalid password. Please try again.' };
+    //     } else {  //email and password correct
+    //       return null;
+    //     }
+    //   }),
+    //   catchError(error => {
+    //     return of({ somethingWrong: 'Invalid response format. Please try again.' });
+    //   })
+    // );
+    return of(null);
   };
 }
